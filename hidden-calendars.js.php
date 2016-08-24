5e85c12d29bd6d2eea421f4a22f95b17ea217a2d
Seth Battis <sethbattis@stmarksschool.org>
2016-08-23 22:05:16 -0400

    renameCalendar()
    ...and also require `common.inc.php` so everything we expect will be there! (Like `$customPrefs`)

--- hidden-calendars.js.php
+++ hidden-calendars.js.php
@@ -1,14 +1,21 @@
 <?php
 
+require_once 'common.inc.php';
+
 $response = $customPrefs->query("
     SELECT *
         FROM `courses`
         WHERE
-            `calendar-visible` = '0'
+            `calendar-visible` = '0' OR
+            `calendar-name` IS NOT NULL
 ");
 
 ?>
  var canvashack = {
+    renameCalendar: function (id, name) {
+       $('#context-list li[data-context="course_' + id + '"] label').text(name);
+    },
+
     hideCalendar: function (id) {
         /* look to see if that calendar is currently enabled */
         if ($('#context-list li[data-context="' + id + '"] checked').length != 0) {
@@ -17,14 +24,19 @@ $response = $customPrefs->query("
         }
 
         /* hide calendar so that it can't be re-enabled -- and won't count against limit of 10 */
-        $('#context-list li[data-context="' + id + '"]').remove();
+        $('#context-list li[data-context="course_' + id + '"]').remove();
     },
 
     hideCalendars: function () {
         <?php
 
-        while (($course = $response->fetch_assoc()) !== false) {
-            echo "hideCalendar({$course['id']});" . PHP_EOL;
+        while ($course = $response->fetch_assoc()) {
+            if ($course['calendar-visible'] == false) {
+                echo "this.hideCalendar({$course['id']});" . PHP_EOL;
+            }
+            if (!empty($course['calendar-name'])) {
+                echo "this.renameCalendar({$course['id']}, '{$course['calendar-name']}');" . PHP_EOL;
+            }
         }
 
         ?>
